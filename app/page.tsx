99f73730b1281312285266044e5e246392c861be
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-03-03 14:32:13 +0530

    initial works

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,190 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import {
+  FaceLandmarker,
+  FaceLandmarkerOptions,
+  FilesetResolver,
+} from "@mediapipe/tasks-vision";
+import { Color, Euler, Matrix4 } from "three";
+import { Canvas, useFrame, useGraph } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import { useDropzone } from "react-dropzone";
+import { DotBackgroundDemo } from "./background";
 
-export default function Home() {
+let video: HTMLVideoElement;
+let faceLandmarker: FaceLandmarker;
+let lastVideoTime = -1;
+let blendshapes: any[] = [];
+let rotation: Euler;
+let headMesh: any[] = [];
+
+const options: FaceLandmarkerOptions = {
+  baseOptions: {
+    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+    delegate: "GPU",
+  },
+  numFaces: 1,
+  runningMode: "VIDEO",
+  outputFaceBlendshapes: true,
+  outputFacialTransformationMatrixes: true,
+};
+
+function Avatar({ url }: { url: string }) {
+  const { scene } = useGLTF(url);
+  const { nodes } = useGraph(scene);
+
+  useEffect(() => {
+    if (nodes.Wolf3D_Head) headMesh.push(nodes.Wolf3D_Head);
+    if (nodes.Wolf3D_Teeth) headMesh.push(nodes.Wolf3D_Teeth);
+    if (nodes.Wolf3D_Beard) headMesh.push(nodes.Wolf3D_Beard);
+    if (nodes.Wolf3D_Avatar) headMesh.push(nodes.Wolf3D_Avatar);
+    if (nodes.Wolf3D_Head_Custom) headMesh.push(nodes.Wolf3D_Head_Custom);
+  }, [nodes, url]);
+
+  useFrame(() => {
+    if (blendshapes.length > 0) {
+      blendshapes.forEach((element) => {
+        headMesh.forEach((mesh) => {
+          let index = mesh.morphTargetDictionary[element.categoryName];
+          if (index >= 0) {
+            mesh.morphTargetInfluences[index] = element.score;
+          }
+        });
+      });
+
+      nodes.Head.rotation.set(rotation.x, rotation.y, rotation.z);
+      nodes.Neck.rotation.set(
+        rotation.x / 5 + 0.3,
+        rotation.y / 5,
+        rotation.z / 5
+      );
+      nodes.Spine2.rotation.set(
+        rotation.x / 10,
+        rotation.y / 10,
+        rotation.z / 10
+      );
+    }
+  });
+
+  return <primitive object={scene} position={[0, -1.75, 3]} />;
+}
+
+function App() {
+  const [show, setShow] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>(
+    "https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024"
+  );
+  const { getRootProps } = useDropzone({
+    onDrop: (files) => {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        setUrl(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    },
+  });
+
+  const setup = async () => {
+    const filesetResolver = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+    );
+    faceLandmarker = await FaceLandmarker.createFromOptions(
+      filesetResolver,
+      options
+    );
+
+    video = document.getElementById("video") as HTMLVideoElement;
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1280, height: 720 },
+        audio: false,
+      })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predict);
+      });
+  };
+
+  const predict = async () => {
+    let nowInMs = Date.now();
+    if (lastVideoTime !== video.currentTime) {
+      lastVideoTime = video.currentTime;
+      const faceLandmarkerResult = faceLandmarker.detectForVideo(
+        video,
+        nowInMs
+      );
+
+      if (
+        faceLandmarkerResult.faceBlendshapes &&
+        faceLandmarkerResult.faceBlendshapes.length > 0 &&
+        faceLandmarkerResult.faceBlendshapes[0].categories
+      ) {
+        blendshapes = faceLandmarkerResult.faceBlendshapes[0].categories;
+
+        const matrix = new Matrix4().fromArray(
+          faceLandmarkerResult.facialTransformationMatrixes![0].data
+        );
+        rotation = new Euler().setFromRotationMatrix(matrix);
+      }
+    }
+
+    window.requestAnimationFrame(predict);
+  };
+
+  const handleOnChange = (event: any) => {
+    setUrl(`${event.target.value}?morphTargets=ARKit&textureAtlas=1024`);
+  };
+
+  useEffect(() => {
+    setup();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-col items-center justify-center gap-5">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+      {/* <div {...getRootProps({ className: "dropzone" })}>
+        <p>Drag & drop RPM avatar GLB file here</p>
+      </div>
+      <input
+        className="url"
+        type="text"
+        placeholder="Paste RPM avatar URL"
+        onChange={handleOnChange}
+      /> */}
+      <div className="w-3/4 md:w-1/2 h-14 z-20 bg-white rounded-3xl">Hello</div>
+      <div className="flex justify-center content-center bg-gradient-to-r from-blue-500 to-purple-500 rounded-3xl h-1/2 w-full md:w-1/2 md:h-1/2 z-20 border-[5px] dark:border-slate-200 border-slate-600">
+        <video className="camera-feed hidden " id="video" autoPlay></video>
+
+        <Canvas
+          style={{ height: "100%", width: "95%" }}
+          camera={{ fov: 20 }}
+          shadows
+        >
+          <ambientLight intensity={0.5} />
+
+          <pointLight
+            position={[10, 10, 10]}
+            color={new Color(1, 1, 0)}
+            intensity={0.5}
+            castShadow
+          />
+          <pointLight
+            position={[-10, 0, 10]}
+            color={new Color(1, 0, 0)}
+            intensity={0.5}
+            castShadow
+          />
+          <pointLight position={[0, 0, 10]} intensity={0.5} castShadow />
+          <Avatar url={url} />
+        </Canvas>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <img className="logo" src="./logo.png" />
+    </div>
   );
 }
+
+export default App;
