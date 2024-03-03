98eb1ccd38cc9dd450bea0c565e8ed2c37fd4b43
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-03-03 17:06:38 +0530

    refactor: detach logic from page.tsx

--- app/components/VideoView/VideoView.tsx
+++ app/components/VideoView/VideoView.tsx
@@ -1,10 +1,78 @@
-import React from "react";
-import { Color } from "three";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Color, Euler, Matrix4 } from "three";
 import { Canvas } from "@react-three/fiber";
 import { Avatar, AvatarProps } from "./components/Avatar";
+import {
+  Category,
+  FaceLandmarker,
+  FilesetResolver,
+} from "@mediapipe/tasks-vision";
+import options from "@/app/helpers/faceLandMarks";
 
-export default function VideoView({ url, blendshapes, rotation }: AvatarProps) {
-    
+export default function VideoView() {
+  let video: HTMLVideoElement;
+  let faceLandmarker: FaceLandmarker;
+  let lastVideoTime = -1;
+  const [blendshapes, setBlendshapes] = useState<Category[]>();
+  const [rotation, setRotation] = useState<Euler>();
+  const [show, setShow] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>(
+    "https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024"
+  );
+  const handleOnChange = (event: any) => {
+    setUrl(`${event.target.value}?morphTargets=ARKit&textureAtlas=1024`);
+  };
+  useEffect(() => {
+    setup();
+  }, []);
+
+  const setup = async () => {
+    const filesetResolver = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+    );
+    faceLandmarker = await FaceLandmarker.createFromOptions(
+      filesetResolver,
+      options
+    );
+
+    video = document.getElementById("video") as HTMLVideoElement;
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1280, height: 720 },
+        audio: false,
+      })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predict);
+      });
+  };
+
+  const predict = async () => {
+    let nowInMs = Date.now();
+    if (lastVideoTime !== video.currentTime) {
+      lastVideoTime = video.currentTime;
+      const faceLandmarkerResult = faceLandmarker.detectForVideo(
+        video,
+        nowInMs
+      );
+
+      if (
+        faceLandmarkerResult.faceBlendshapes &&
+        faceLandmarkerResult.faceBlendshapes.length > 0 &&
+        faceLandmarkerResult.faceBlendshapes[0].categories
+      ) {
+        setBlendshapes(faceLandmarkerResult.faceBlendshapes[0].categories);
+
+        const matrix = new Matrix4().fromArray(
+          faceLandmarkerResult.facialTransformationMatrixes![0].data
+        );
+        setRotation(new Euler().setFromRotationMatrix(matrix));
+      }
+    }
+
+    window.requestAnimationFrame(predict);
+  };
   return (
     <div className="flex justify-center content-center items-center bg-gradient-to-r from-blue-500 to-purple-500 rounded-3xl w-full min-h-[50%] md:max-w-xl z-20 border-[5px] dark:border-slate-200 border-slate-600">
       <video
--- app/page.tsx
+++ app/page.tsx
@@ -1,97 +1,8 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-import {
-  Category,
-  FaceLandmarker,
-  FilesetResolver,
-} from "@mediapipe/tasks-vision";
-import { Color, Euler, Matrix4 } from "three";
-import { Canvas, useFrame, useGraph } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
-import { useDropzone } from "react-dropzone";
-import { DotBackgroundDemo } from "./background";
-import options from "./helpers/faceLandMarks";
-import { Avatar } from "./components/VideoView/components/Avatar";
 import VideoView from "./components/VideoView/VideoView";
-
-let video: HTMLVideoElement;
-let faceLandmarker: FaceLandmarker;
-let lastVideoTime = -1;
 
 // const headMesh: any[] = [];
 
 function App() {
-  const [blendshapes, setBlendshapes] = useState<Category[]>();
-  const [rotation, setRotation] = useState<Euler>();
-  const [show, setShow] = useState<boolean>(false);
-  const [url, setUrl] = useState<string>(
-    "https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024"
-  );
-  const { getRootProps } = useDropzone({
-    onDrop: (files) => {
-      const file = files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        setUrl(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    },
-  });
-  useEffect(() => {
-    setup();
-  }, []);
-
-  const setup = async () => {
-    const filesetResolver = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
-    );
-    faceLandmarker = await FaceLandmarker.createFromOptions(
-      filesetResolver,
-      options
-    );
-
-    video = document.getElementById("video") as HTMLVideoElement;
-    navigator.mediaDevices
-      .getUserMedia({
-        video: { width: 1280, height: 720 },
-        audio: false,
-      })
-      .then(function (stream) {
-        video.srcObject = stream;
-        video.addEventListener("loadeddata", predict);
-      });
-  };
-
-  const predict = async () => {
-    let nowInMs = Date.now();
-    if (lastVideoTime !== video.currentTime) {
-      lastVideoTime = video.currentTime;
-      const faceLandmarkerResult = faceLandmarker.detectForVideo(
-        video,
-        nowInMs
-      );
-
-      if (
-        faceLandmarkerResult.faceBlendshapes &&
-        faceLandmarkerResult.faceBlendshapes.length > 0 &&
-        faceLandmarkerResult.faceBlendshapes[0].categories
-      ) {
-        setBlendshapes(faceLandmarkerResult.faceBlendshapes[0].categories);
-
-        const matrix = new Matrix4().fromArray(
-          faceLandmarkerResult.facialTransformationMatrixes![0].data
-        );
-        setRotation(new Euler().setFromRotationMatrix(matrix));
-      }
-    }
-
-    window.requestAnimationFrame(predict);
-  };
-
-  const handleOnChange = (event: any) => {
-    setUrl(`${event.target.value}?morphTargets=ARKit&textureAtlas=1024`);
-  };
-
   return (
     <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-col items-center justify-center gap-5">
       {/* Radial gradient for the container to give a faded look */}
@@ -106,11 +17,7 @@ function App() {
         placeholder="Paste RPM avatar URL"
         onChange={handleOnChange}
       /> */}
-      <VideoView
-        url={url}
-        blendshapes={blendshapes}
-        rotation={rotation}
-      ></VideoView>
+      <VideoView></VideoView>
     </div>
   );
 }
