e23e1155ac52dc25972dab07d0810e0c4ef8f256
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-03-03 16:07:54 +0530

    refactor: separate avatar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Category,
   FaceLandmarker,
-  FaceLandmarkerOptions,
   FilesetResolver,
 } from "@mediapipe/tasks-vision";
 import { Color, Euler, Matrix4 } from "three";
@@ -12,56 +11,13 @@ import { useGLTF } from "@react-three/drei";
 import { useDropzone } from "react-dropzone";
 import { DotBackgroundDemo } from "./background";
 import options from "./helpers/faceLandMarks";
-interface AvatarProps {
-  url: string;
-  blendshapes: Category[] | undefined; // Assuming blendshapes is an array of objects
-  rotation: Euler | undefined;
-}
+import { Avatar } from "./components/Avatar";
+
 let video: HTMLVideoElement;
 let faceLandmarker: FaceLandmarker;
 let lastVideoTime = -1;
 
-let headMesh: any[] = [];
-function Avatar({ url, blendshapes, rotation }: AvatarProps) {
-  const { scene } = useGLTF(url);
-  const { nodes } = useGraph(scene);
-
-  useEffect(() => {
-    if (nodes.Wolf3D_Head) headMesh.push(nodes.Wolf3D_Head);
-    if (nodes.Wolf3D_Teeth) headMesh.push(nodes.Wolf3D_Teeth);
-    if (nodes.Wolf3D_Beard) headMesh.push(nodes.Wolf3D_Beard);
-    if (nodes.Wolf3D_Avatar) headMesh.push(nodes.Wolf3D_Avatar);
-    if (nodes.Wolf3D_Head_Custom) headMesh.push(nodes.Wolf3D_Head_Custom);
-  }, [nodes, url]);
-
-  useFrame(() => {
-    if (blendshapes && blendshapes?.length > 0) {
-      blendshapes?.forEach((element) => {
-        headMesh.forEach((mesh) => {
-          let index = mesh.morphTargetDictionary[element.categoryName];
-          if (index >= 0) {
-            mesh.morphTargetInfluences[index] = element.score;
-          }
-        });
-      });
-      if (rotation) {
-        nodes.Head.rotation.set(rotation.x, rotation.y, rotation.z);
-        nodes.Neck.rotation.set(
-          rotation.x / 5 + 0.3,
-          rotation.y / 5,
-          rotation.z / 5
-        );
-        nodes.Spine2.rotation.set(
-          rotation.x / 10,
-          rotation.y / 10,
-          rotation.z / 10
-        );
-      }
-    }
-  });
-
-  return <primitive object={scene} position={[0, -1.75, 3]} />;
-}
+const headMesh: any[] = [];
 
 function App() {
   const [blendshapes, setBlendshapes] = useState<Category[]>();
@@ -80,6 +36,9 @@ function App() {
       reader.readAsDataURL(file);
     },
   });
+  useEffect(() => {
+    setup();
+  }, []);
 
   const setup = async () => {
     const filesetResolver = await FilesetResolver.forVisionTasks(
@@ -132,10 +91,6 @@ function App() {
     setUrl(`${event.target.value}?morphTargets=ARKit&textureAtlas=1024`);
   };
 
-  useEffect(() => {
-    setup();
-  }, []);
-
   return (
     <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-col items-center justify-center gap-5">
       {/* Radial gradient for the container to give a faded look */}
@@ -150,7 +105,7 @@ function App() {
         placeholder="Paste RPM avatar URL"
         onChange={handleOnChange}
       /> */}
-      <div className="w-3/4 md:w-1/2 h-14 z-20 bg-white rounded-3xl">Hello</div>
+
       <div className="flex justify-center content-center bg-gradient-to-r from-blue-500 to-purple-500 rounded-3xl h-1/2 w-full md:w-1/2 md:h-1/2 z-20 border-[5px] dark:border-slate-200 border-slate-600">
         <video className="camera-feed hidden " id="video" autoPlay></video>
 
@@ -175,10 +130,17 @@ function App() {
           />
           <pointLight position={[0, 0, 10]} intensity={0.5} castShadow />
 
-          <Avatar url={url} blendshapes={blendshapes} rotation={rotation} />
+          <Avatar
+            url={url}
+            blendshapes={blendshapes}
+            rotation={rotation}
+            // headMesh={headMesh}
+          />
         </Canvas>
+        {/* <div className="reletive top-0 z-50">
+          <div className=" bg-white rounded-3xl">Hello</div>
+        </div> */}
       </div>
-
       {/* <img className="logo" src="./logo.png" /> */}
     </div>
   );
