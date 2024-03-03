83a020d2c953825ba40f3840519d8602678667e3
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-03-03 16:11:25 +0530

    style: App screen

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ function App() {
   };
 
   return (
-    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-col items-center justify-center gap-5">
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-col items-center justify-center gap-5">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
