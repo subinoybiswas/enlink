3fc4594e40d79620773d362a103d0cab3d11ee84
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-03-06 00:51:06 +0530

    milestone: lobbypage ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import VideoView from "../components/VideoView/VideoView";
 import { Divider } from "@mui/joy";
-import { Avatar, Button, DropdownTrigger } from "@nextui-org/react";
+import {
+  Avatar,
+  Button,
+  DropdownTrigger,
+  AvatarGroup,
+} from "@nextui-org/react";
 import { NextUIProvider } from "@nextui-org/react";
 import { HeartIcon } from "./assets/hearticon";
 import { Dropdown, DropdownMenu, DropdownItem } from "@nextui-org/react";
@@ -24,11 +29,11 @@ function App() {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-row  gap-10 z-20 p-5">
+        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.5] bg-dot-black/[0.5] relative flex flex-row  gap-10 z-20 p-4">
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] z-[-1]"></div>
 
-          <div className=" w-1/2 flex flex-col text-center gap-2 content-center  font-myFont ">
+          <div className=" w-[55%] flex flex-col text-center gap-2 content-center  font-myFont h-full">
             <h1 className="font-myFont text-3xl my-2">Setup your Meet</h1>
             <div className="grid grid-flow-col gap-1 w-full   self-center place-items-center bg-slate-400/50 p-2 rounded-3xl">
               {/* <User
@@ -65,12 +70,19 @@ function App() {
               </Snippet>
             </div>
           </div>
-          <div className="w-1/2">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius et
-              nemo atque ea eaque magni, blanditiis eos, amet minima unde odio.
-              Maxime eius voluptas delectus amet quas id iusto incidunt.
-            </p>
+          <div className="w-[45%] text-center flex flex-col content-center gap-5 h-full justify-center">
+            <h1 className="text-4xl self-center">Are you Ready?</h1>
+            <AvatarGroup isBordered>
+              <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+              <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+              <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+              <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+              <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+              <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+            </AvatarGroup>
+            <div>
+              <Button size="lg">Join</Button>
+            </div>
           </div>
         </div>
       </NextThemesProvider>
