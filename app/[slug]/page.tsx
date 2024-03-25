c05ac57702b62c244f366bc62246adf56a723a23
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-03-25 15:33:36 +0530

    style: change layout

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { Button } from "@nextui-org/react";
 import React, { useState } from "react";
+import { Button } from "@nextui-org/react";
 import { FaMicrophone, FaVideo } from "react-icons/fa";
 import { FiCamera } from "react-icons/fi";
 import { HiSparkles } from "react-icons/hi";
 import { IoMdExit } from "react-icons/io";
 import { SlCalender } from "react-icons/sl";
 import { MdCallEnd } from "react-icons/md";
+import { IoIosSettings } from "react-icons/io";
 export default function MeetingPage({ params }: { params: { slug: string } }) {
   const [isSettingPanelOpen, setIsSettingPanelOpen] = useState(false);
 
@@ -16,13 +17,9 @@ export default function MeetingPage({ params }: { params: { slug: string } }) {
 
   return (
     <div className="flex flex-col max-h-screen min-h-screen overflow-hidden">
-      <div className="flex flex-row w-full h-[50px] bg-slate-50">
-        <div className="w-1/3 h-full bg-black bg-opacity-50">sds</div>
-        <div className="w-2/3 h-full bg-black bg-opacity-50">dsd</div>
-        <div className="w-2/3 h-full bg-black bg-opacity-50">dsd</div>
-      </div>
+      <div className="h-[50px] m-2 bg-slate-400 rounded-3xl">Hi </div>
       <div className="bg-slate-300 flex-1 flex flex-row">
-        <div className=" flex flex-col bg-slate-800 justify-between justify-items-center w-[60px] text-center rounded-3xl">
+        {/* <div className=" flex flex-col bg-slate-800 justify-between justify-items-center w-[60px] text-center rounded-3xl">
           <div className="invisible">Start</div>
           <div className="grid gap-4 justify-items-center justify-center">
             <Button isIconOnly radius="full" className="justify-self-center bg">
@@ -40,17 +37,29 @@ export default function MeetingPage({ params }: { params: { slug: string } }) {
               <IoMdExit></IoMdExit>
             </Button>
           </div>
-        </div>
-        <div className="bg-slate-400 flex-1">
-          Body
-          <div className="grid grid-flow-col gap-0.5 w-1/4   self-center place-items-center bg-slate-800/50 p-1 rounded-3xl ">
-            <Button
-              radius="full"
-              variant="faded"
-              isIconOnly
-              aria-label="Like"
+        </div> */}
+        <div className="flex flex-col bg-slate-400 flex-1 text-center">
+          <div className="flex-1 flex flex-row">
+            <div
+              className="flex-1"
               style={{ transition: "all 0.5s ease-in-out" }}
             >
+              Body
+            </div>
+            <div
+              style={{
+                width: isSettingPanelOpen ? "0" : "25vw",
+                opacity: isSettingPanelOpen ? 0 : 1,
+                visibility: isSettingPanelOpen ? "hidden" : "visible",
+                transition: "width 0.5s ease-in-out",
+              }}
+              className="bg-slate-300 rounded-3xl m-2"
+            >
+              <div onClick={toggleSettingPanel}>Close</div>
+            </div>
+          </div>
+          <div className="grid grid-flow-col gap-2 mb-2 self-center place-items-center bg-slate-800/50 p-1 rounded-3xl ">
+            <Button radius="full" variant="faded" isIconOnly aria-label="Like">
               <HiSparkles size={20} />
             </Button>
             <Button radius="full" variant="faded" isIconOnly aria-label="Like">
@@ -69,27 +78,17 @@ export default function MeetingPage({ params }: { params: { slug: string } }) {
             <Button radius="full" variant="faded" isIconOnly aria-label="Like">
               <FaMicrophone size={20} />
             </Button>
-            <Button radius="full" variant="faded" isIconOnly aria-label="Like">
-              <FaMicrophone size={20} />
+            <Button
+              radius="full"
+              variant="faded"
+              isIconOnly
+              aria-label="Like"
+              onClick={toggleSettingPanel}
+            >
+              <IoIosSettings size={20} />
             </Button>
           </div>
         </div>
-      </div>
-      <div
-        className={`fixed top-0 right-0 h-full min-w-[30vw] bg-white z-10 ${
-          isSettingPanelOpen ? "translate-x-0" : "translate-x-full"
-        } transition-transform ease-in-out duration-300 bg-orange-500`}
-      >
-        <button className="absolute top-4 right-4" onClick={toggleSettingPanel}>
-          Close
-        </button>
-        <div className="p-4">
-          {/* Add your setting panel content here */}
-          Setting Panel Content
-        </div>
-      </div>
-      <div className="absolute top-4 right-4">
-        <button onClick={toggleSettingPanel}>Open Settings</button>
       </div>
     </div>
   );
