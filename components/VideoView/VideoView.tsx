3eff98f4b1b4d910cd97d565d079662b08ac0f91
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-06-23 00:03:06 +0530

    cleanedup beta

--- components/VideoView/VideoView.tsx
+++ components/VideoView/VideoView.tsx
@@ -79,20 +79,14 @@ export default function VideoView({
     window.requestAnimationFrame(predict);
   };
   const avatarRef = createRef<HTMLCanvasElement>();
-  const canvasRef = useRef(null);
-  const videoRef = useRef(null);
   const recordButtonRef = useRef(null);
-  const playButtonRef = useRef(null);
-  const downloadButtonRef = useRef(null);
 
-  const [recordedBlobs, setRecordedBlobs] = useState([]);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
-  const [stream, setStream] = useState(null);
+  const [recordedBlobs, setRecordedBlobs] = useState<Blob[]>([]);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
+  const [stream, setStream] = useState<MediaStream>();
 
   useEffect(() => {
     const canvas = avatarRef.current;
-    const video = videoRef.current;
-
     // Capture stream from canvas
     const canvasStream = canvas?.captureStream();
     setStream(canvasStream);
@@ -107,33 +101,31 @@ export default function VideoView({
     };
   }, []);
 
-  const handleDataAvailable = (event) => {
+  const handleDataAvailable = (event: any) => {
     if (event.data && event.data.size > 0) {
       setRecordedBlobs((prevBlobs) => [...prevBlobs, event.data]);
     }
   };
 
-  const handleStop = (event) => {
+  const handleStop = (event: any) => {
     console.log("Recorder stopped: ", event);
-    const superBuffer = new Blob(recordedBlobs, { type: "video/webm" });
-    videoRef.current.src = window.URL.createObjectURL(superBuffer);
   };
 
   const toggleRecording = () => {
-    if (recordButtonRef.current.textContent === "Start Recording") {
-      startRecording();
-    } else {
-      stopRecording();
-      recordButtonRef.current.textContent = "Start Recording";
-      playButtonRef.current.disabled = false;
-      downloadButtonRef.current.disabled = false;
-    }
+    startRecording();
+  };
+  const toggleRecordingOff = () => {
+    stopRecording();
   };
 
   const startRecording = () => {
     let options = { mimeType: "video/webm" };
     setRecordedBlobs([]);
     try {
+      if (stream === undefined) {
+        console.log("Stream is undefined");
+        return;
+      }
       let recorder = new MediaRecorder(stream, options);
       setMediaRecorder(recorder);
     } catch (e0) {
@@ -145,9 +137,9 @@ export default function VideoView({
       "with options",
       options
     );
-    recordButtonRef.current.textContent = "Stop Recording";
-    playButtonRef.current.disabled = true;
-    downloadButtonRef.current.disabled = true;
+    if (mediaRecorder === undefined) {
+      return;
+    }
     mediaRecorder.onstop = handleStop;
     mediaRecorder.ondataavailable = handleDataAvailable;
     mediaRecorder.start(100); // collect 100ms of data
@@ -155,28 +147,16 @@ export default function VideoView({
   };
 
   const stopRecording = () => {
+    if (mediaRecorder === undefined) {
+      console.log("MediaRecorder is undefined");
+      return;
+    }
     mediaRecorder.stop();
     console.log("Recorded Blobs: ", recordedBlobs);
-    videoRef.current.controls = true;
-  };
-
-  const play = () => {
-    videoRef.current.play();
-  };
-
-  const download = () => {
     const blob = new Blob(recordedBlobs, { type: "video/webm" });
     const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.style.display = "none";
-    a.href = url;
-    a.download = "test.webm";
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(() => {
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    }, 100);
+    console.log("url", url);
+    // videoRef.current.controls = true;
   };
 
   return (
@@ -185,14 +165,17 @@ export default function VideoView({
       style={{ aspectRatio: 16 / 9, transition: "all 0.5s ease-in-out" }}
     >
       <Loader />
-      <p>{status}</p>
-      <br />
-      <button ref={recordButtonRef} onClick={toggleRecording}>Start Recording</button>
-      <button ref={playButtonRef} onClick={play} disabled>Play</button>
-      <button ref={downloadButtonRef} onClick={download} disabled>Download</button>
-      <br />
-      <video ref={videoRef} width={400} height={300} controls />
 
+      <br />
+      <button ref={recordButtonRef} onClick={toggleRecording}>
+        Start Recording
+      </button>
+      <button ref={recordButtonRef} onClick={toggleRecordingOff}>
+        Stop Recording
+      </button>
+
+      <br />
+      <video width={500} height={600} controls />
       <video
         className="camera-feed"
         style={{
